494305b594b0857cfb24fd929545405bff0406f6
PabloOsorno <pablosornochavarria06@gmail.com>
2025-11-24 22:13:18 -0600

    Prueba 3 de migraciones en producciones

---
+++ backend/database/migrations/2025_11_25_030000_create_test_pipeline_table.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::create('test_pipeline', function (Blueprint $table) {
+            $table->id();
+            $table->string('descripcion')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('test_pipeline');
+    }
+};
