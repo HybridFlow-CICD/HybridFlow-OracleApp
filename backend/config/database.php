9fe031e402de1cc4c2b6d3852f9d67b30c13e3e3
Jose Pablo Osorno Chavarria <pablosornochavarria06@gmail.com>
2025-11-28 22:08:15 -0600

    Update Oracle database configuration parameters

--- backend/config/database.php
+++ backend/config/database.php
@@ -93,20 +93,18 @@ return [
             // 'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'false'),
         ],
 
-        'oracle' => [
+'oracle' => [
     'driver'        => 'oracle',
-    'tns'           => env('DB_TNS', ''),
-    'host'          => env('DB_HOST'),
+    'host'          => env('DB_HOST', ''),
     'port'          => env('DB_PORT', '1521'),
-    'database'      => env('DB_DATABASE', 'orclpdb'),
+    'database'      => env('DB_DATABASE', ''),
     'service_name'  => env('DB_SERVICE_NAME', 'orclpdb'),
-    'username'      => env('DB_USERNAME'),
-    'password'      => env('DB_PASSWORD'),
-    'charset'       => env('DB_CHARSET', 'AL32UTF8'),
-    'prefix'        => env('DB_PREFIX', ''),
-    'prefix_schema' => env('DB_SCHEMA_PREFIX', ''),
-    'server_version'=> env('DB_SERVER_VERSION', '19.3'),
+    'username'      => env('DB_USERNAME', 'GRUPO02'),
+    'password'      => env('DB_PASSWORD', 'Grupo02*'),
+    'charset'       => 'AL32UTF8',
+    'prefix'        => '',
 ],
+
 
 
     ],
